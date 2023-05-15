02f57c79d533423b4fc91388d2ace07616dd29ca
thaariqNasution <Thaariqnst@gmail.com>
2023-05-15 10:44:13 +0700

    view routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 }); 
+
+Route::view('/signin','signin');
+Route::view('/login','login');
+Route::view('/history','history');
+
