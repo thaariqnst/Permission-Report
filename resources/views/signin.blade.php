b505cd0201af6e8c44605af929551105580e0062
thaariqNasution <Thaariqnst@gmail.com>
2023-05-16 08:44:14 +0700

    Tailwind Installed

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -5,8 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
+    
+    <div class="container bg-black rounded-lg">
+        <h1 class="text-white p-4">Hello test</h1>
+    </div>
+
     
 </body>
 </html>
