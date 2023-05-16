b7fac3444058532abc2253584bf80307b4cba83d
thaariqNasution <Thaariqnst@gmail.com>
2023-05-16 10:55:52 +0700

    Input Page UI

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,12 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Permission Input</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+
+<div class="">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">Submit Permission</div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label class="label-form">Name</label>
+                            <input type="text" name="name" class="form-control">
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="label-form">Class</label>
+                            <select name="class" class="form-control">
+                                
+                                <option value=""></option> 
+                                
+                            </select>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="label-form">Reason</label>
+                            <textarea type="text" name="reason" class="form-control" rows="3"></textarea>
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="label-form">Start Date</label>
+                            <input type="date" name="start_date" class="form-control">
+                        </div>
+                        <div class="form-group mt-3">
+                            <label class="label-form">End Date</label>
+                            <input type="date" name="end_date" class="form-control">
+                        </div>
+                        <div class="form-group mt-3">
+                            <button class="btn btn-success">Submit Permission</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    
-    <div class="container bg-black rounded-lg">
-        <h1 class="text-white p-4">Hello test</h1>
-    </div>
-
-    
-</body>
-</html>
+@extends('layouts.app')
