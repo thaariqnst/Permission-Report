67819866cdfdfb64145722263e83bb01b6b09fd5
thaariqNasution <Thaariqnst@gmail.com>
2023-05-24 15:08:35 +0700

    need to fix permit card

--- resources/views/permit/index.blade.php
+++ resources/views/permit/index.blade.php
@@ -13,23 +13,25 @@
                                 <div class="fw-bold">Muhammad Ath Thaariq</div>
                                 <div class="fw-italic">XI RPL A</div>
                             </div>
-                            <div class="card-body p-4">
-                                <div class="col-md-12 d-flex mb-3">
+                            <div class="card-body p-4 d-flex">
+                                <div class="mb-3">
                                     <div class="col-md-6 pe-3">
                                         <div class="fw-light">permit type</div>
                                         <div>Health</div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="fw-light">start date</div>
-                                        <div>24-05-2023</div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12 d-flex">
                                     <div class="col-md-6 pe-3">
                                         <div class="fw-light">description</div>
                                         <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit beatae, totam natus at quam tempore!</div>
                                     </div>
-                                    <div class="col-md-6">
+                                    
+                                </div>
+                                
+                                <div class="">
+                                <div class="">
+                                        <div class="fw-light">start date</div>
+                                        <div>24-05-2023</div>
+                                    </div>
+                                    <div class="">
                                         <div class="fw-light">end date</div>
                                         <div>27-05-2023</div>
                                     </div>
@@ -42,41 +44,7 @@
                         </div>
                     </div>
 
-                    <div class="container">
-                        <div class="card">
-                            <div class="card-header bg-white">
-                                <div class="fw-bold">Muhammad Ath Thaariq</div>
-                                <div class="fw-italic">XI RPL A</div>
-                            </div>
-                            <div class="card-body p-4">
-                                <div class="col-md-12 d-flex mb-3">
-                                    <div class="col-md-6 pe-3">
-                                        <div class="fw-light">permit type</div>
-                                        <div>Health</div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="fw-light">start date</div>
-                                        <div>24-05-2023</div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12 d-flex">
-                                    <div class="col-md-6 pe-3">
-                                        <div class="fw-light">description</div>
-                                        <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit beatae, totam natus at quam tempore!</div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="fw-light">end date</div>
-                                        <div>27-05-2023</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-footer bg-white">
-                                <div class="fw-light">PIC Name</div>
-                                <div class="fw-bold">Abdul Jabbar</div>
-                            </div>
-                        </div>
-                    </div>
-
+    
                     
                     
 
