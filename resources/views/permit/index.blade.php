ae7a907be0c4fe4454dc2aa5324bbb37f420aed4
thaariqNasution <Thaariqnst@gmail.com>
2023-05-24 11:19:40 +0700

    almost complete on recent page

--- resources/views/permit/index.blade.php
+++ resources/views/permit/index.blade.php
@@ -3,23 +3,85 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-5">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header"> Sign In</div>
-                <div class="card-body">
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <label class="label-form">Username</label>
-                            <input type="text" name="username" class="form-control">
+                <div class="card-header">Your Recent Submission</div>
+                <div class="card-body d-flex ">
+                    <div class="container">
+                        <div class="card">
+                            <div class="card-header bg-white">
+                                <div class="fw-bold">Muhammad Ath Thaariq</div>
+                                <div class="fw-italic">XI RPL A</div>
+                            </div>
+                            <div class="card-body p-4">
+                                <div class="col-md-12 d-flex mb-3">
+                                    <div class="col-md-6 pe-3">
+                                        <div class="fw-light">permit type</div>
+                                        <div>Health</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="fw-light">start date</div>
+                                        <div>24-05-2023</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12 d-flex">
+                                    <div class="col-md-6 pe-3">
+                                        <div class="fw-light">description</div>
+                                        <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit beatae, totam natus at quam tempore!</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="fw-light">end date</div>
+                                        <div>27-05-2023</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <div class="fw-light">PIC Name</div>
+                                <div class="fw-bold">Abdul Jabbar</div>
+                            </div>
                         </div>
-                        <div class="form-group mt-3">
-                            <label class="label-form">Password</label>
-                            <input type="text" name="password" class="form-control">
+                    </div>
+
+                    <div class="container">
+                        <div class="card">
+                            <div class="card-header bg-white">
+                                <div class="fw-bold">Muhammad Ath Thaariq</div>
+                                <div class="fw-italic">XI RPL A</div>
+                            </div>
+                            <div class="card-body p-4">
+                                <div class="col-md-12 d-flex mb-3">
+                                    <div class="col-md-6 pe-3">
+                                        <div class="fw-light">permit type</div>
+                                        <div>Health</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="fw-light">start date</div>
+                                        <div>24-05-2023</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12 d-flex">
+                                    <div class="col-md-6 pe-3">
+                                        <div class="fw-light">description</div>
+                                        <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit beatae, totam natus at quam tempore!</div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="fw-light">end date</div>
+                                        <div>27-05-2023</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-white">
+                                <div class="fw-light">PIC Name</div>
+                                <div class="fw-bold">Abdul Jabbar</div>
+                            </div>
                         </div>
-                        <div class="form-group mt-3">
-                            <button class="btn btn-success">Daftar</button>
-                        </div>
-                    </form>
+                    </div>
+
+                    
+                    
+
+                    
+                    
                 </div>
             </div>
         </div>
